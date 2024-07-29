0a6fdf62314e7982a5b14dd44874417dbb84238e
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-07-29 10:16:18 +0530

    refactor: Update App.js to fix typo in button text and add styling to reset button

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
   }
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
-      <div className="text-[#0398d4] font-medium text-2xl">Increment && Decrement</div>
+      <div className="text-[#0398d4] font-medium text-2xl">Increment & Decrement</div>
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
         <button onClick={decrement} className = 'border-r-2 text-center w-20 border-[#bfbfbf] text-5xl'> - </button>
         <div className="font-bold ">{count}</div>
         <button onClick={increment} className = 'border-l-2 text-center w-20 border-[#bfbfbf] text-5xl'> + </button>
       </div>
-      <button onClick={reset}>Reset</button>
+      <button onClick={reset} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">Reset</button>
     </div>
   );
 }
